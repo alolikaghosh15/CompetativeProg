dfe58f91fe318318b4378aeddc622f9e40762ee2
alolikaghosh15 <alolikaghosh15@gmail.com>
2026-09-17 16:35:25 +0530

    Create Column.c

---
+++ Column.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int base, i, j, rows, cols, w, address;
+    printf("Enter base address: ");
+    scanf("%d", &base);
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    printf("Enter row and column index: ");
+    scanf("%d %d", &i, &j);
+    printf("Enter size of each element in bytes: ");
+    scanf("%d", &w);
+    address = base + ((j * rows) + i) * w;
+    printf("Address in Column Major Order = %d", address);
+    return 0;
+}
